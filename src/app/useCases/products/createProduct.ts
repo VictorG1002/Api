8c15be6b31cc8f3bc8a2df9684fb701e5925ccce
Victor Oliveira <vggaia10@gmail.com>
2024-02-01 22:45:58 -0300

    add createProduct

--- src/app/useCases/products/createProduct.ts
+++ src/app/useCases/products/createProduct.ts
@@ -6,6 +6,23 @@ import { Product } from '../../models/Product';
 export async function createProduct(req: Request, res: Response) {
   try {
 
+    const imagePath = req.file?.filename;
+
+    const { name , description, price, category , ingredients} = req.body;
+
+
+    const product = await Product.create({
+      name,
+      description,
+      price: Number(price),
+      category,
+      ingredients: JSON.parse(ingredients),
+      imagePath,
+    });
+
+
+    res.status(201).json(product);
+
   } catch (error) {
     console.log(error);
     return res.status(400).json({ error: 'Algo deu errado' });
