9ac0e8a269565914b40d90e6fa826e8b47547cc2
Victor Oliveira <vggaia10@gmail.com>
2024-03-11 22:02:40 -0300

    add new route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import path from 'node:path';
+
 import mongoose from 'mongoose';
 
 import { router } from './router';
@@ -10,6 +12,8 @@ mongoose
     const app = express();
     const port = 3002;
 
+
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
 
